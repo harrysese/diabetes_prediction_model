840a550228ad66be2a4e77773a51696a6b3c2e3d
harrysese <tokonisese@gmail.com>
2024-12-01 15:47:34 +0100

    added guniconr

--- diabetes_predictor/settings.py
+++ diabetes_predictor/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS=[BASE_DIR/ 'static']
-STATIC_ROOT=os.path.join(BASE_DIR, 'static')
+STATIC_ROOT=os.path.join(BASE_DIR, 'staticfiles')
 AUTH_USER_MODEL = 'diabetes_predictor_app.User'
 
 # Default primary key field type
