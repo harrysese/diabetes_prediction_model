eef5094faf37061982a1188f2c3895d6dff65ae0
harrysese <tokonisese@gmail.com>
2024-12-01 14:55:34 +0100

    added static root

--- diabetes_predictor/settings.py
+++ diabetes_predictor/settings.py
@@ -120,6 +120,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS=[BASE_DIR/ 'static']
+STATIC_ROOT=os.path.join(BASE_DIR, 'staic')
 AUTH_USER_MODEL = 'diabetes_predictor_app.User'
 
 # Default primary key field type
